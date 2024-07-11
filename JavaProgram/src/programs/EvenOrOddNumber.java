269af6b7029b0ae04a64e138cd0851fbd49bf22e
devinder90kkr <devinder90kkr@gmail.com>
2024-07-11 22:59:32 +0530

    added extent report

---
+++ JavaProgram/src/programs/EvenOrOddNumber.java
@@ -0,0 +1,17 @@
+package programs;
+
+	public class EvenOrOddNumber
+	{
+			public static void main(String[] args)
+			{
+				int num = 9;
+				if (num % 2 == 0)
+			{
+				System.out.println("Number is even number");
+			}
+				else
+			{
+				System.out.println("Number is odd number");
+				}
+			}
+	}
